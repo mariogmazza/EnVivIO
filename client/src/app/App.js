e3dac70465ff1a7d5f89101c56eb4da668b3c0ac
mario mazza <mario.mazza@rfsmart.com>
2019-12-23 12:51:40 -0500

    ini commit scaffolding

---
+++ client/src/app/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import MainLayout from "../container/MainLayout";
+
+function App() {
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <MainLayout />
+    </React.Fragment>
+  );
+}
+
+export default App;
